34e0accc9571eb11fd89197a7f886a5750876ffc
Raymond Cornely <cornely06@gmail.com>
2021-03-27 10:29:06 -0400

    Add cipher, readme, and gitignore

---
+++ README.md
@@ -0,0 +1,5 @@
+The Odin Project
+Ruby Programming
+PROJECT: CAESAR CIPHER
+
+The goal of this project is to create a caesar cipher using Ruby.
---
+++ caesar_cipher.rb
@@ -0,0 +1,31 @@
+def caesar_cipher(string, shift)
+	return string if shift == 0
+	string_array = string.split("")
+	shift_array = string_array.map do |letter|
+		if ("a".."z").include?(letter)
+			to_ascii = letter.ord
+			shifted = to_ascii + shift
+			while shifted > 122 do 
+              shifted -= 26
+            end
+            while shifted < 97 do 
+              shifted += 26
+            end
+            shifted = shifted.chr
+        elsif ("A".."Z").include?(letter)
+			to_ascii = letter.ord
+			shifted = to_ascii + shift
+			while shifted > 90 do 
+              shifted -= 26
+            end
+            while shifted < 65 do 
+              shifted += 26
+            end
+            shifted = shifted.chr
+        else letter
+        end
+end
+	shift_array.join("")
+end
+
+p caesar_cipher("hey", 5)
